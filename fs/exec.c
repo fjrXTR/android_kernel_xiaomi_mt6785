38a2f9327d27c0d82816c7bd33a125a657f2588c
RyuujiX <saputradenny712@gmail.com>
2023-06-29 09:46:08 +0000

    fs: Use KernelSU Second Way Implementation - Seems KProbes is broken. - Based on https://kernelsu.org/guide/how-to-integrate-for-non-gki.html.
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Change-Id: Id9d887b069aa4c0e63d7579b9529c0741d35d31f
    Signed-off-by: Neebe3289 <neebexd@gmail.com>
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1715,6 +1715,12 @@ extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 /*
  * sys_execve() executes a new program.
  */
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1726,10 +1732,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
